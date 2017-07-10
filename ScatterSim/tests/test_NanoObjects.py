71290581aa54d137c9e197a972d67c86da889004
Julien L <jrmlhermitte@gmail.com>
2017-07-10 16:25:03 -0400

    Added the add operator capability

--- ScatterSim/tests/test_NanoObjects.py
+++ ScatterSim/tests/test_NanoObjects.py
@@ -1,7 +1,8 @@
 from ..NanoObjects import SphereNanoObject
-from numpy.testing import assert_almost_equal
 import numpy as np
+
 from nose.tools import assert_raises
+from numpy.testing import assert_almost_equal, assert_array_almost_equal
 
 # tests should just be functions that return errors
 # if behaviour is not as expected
@@ -20,3 +21,23 @@ def test_sphereNanoObject():
     assert_raises(ValueError, sphere.V, q.T)
 
 
+def test_add():
+    ''' Test the adding of NanoObjects'''
+
+    pargs1 = {'radius' : 10}
+    sphere1 = SphereNanoObject(pargs1)
+    pargs2 = {'radius' : 10, 'x0': 10}
+    sphere2 = SphereNanoObject(pargs2)
+
+    sphere3 = sphere1 + sphere2
+
+    x = np.linspace(-100, 100, 100)
+    X, Y = np.meshgrid(x,x)
+    R = np.array([X, Y, X*0])
+
+    V3 = sphere3.V(R)
+
+    assert_array_almost_equal(V3[40:60, 50], np.array([0., 0., 0., 0., 0., 15.,
+                                                       15., 15., 30., 30., 30.,
+                                                       30., 15., 15., 15., 0.,
+                                                       0., 0., 0., 0.]))
